2d59e48895a6c98133a772dd5dca854d45efb1b0
caracarton <46607249+caracarton@users.noreply.github.com>
2026-03-17 20:45:20 +0100

    se me había olvidado sincronizar el deploy.sh
    He añadido un poco más de explicaciones al código, pero no quiero explicar cada pequeño detalle como "ponemos el nombre de dominio en domain name", me he centrado más en explicar cosas más técnicas

--- Ansible/deploy.sh
+++ Ansible/deploy.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+#en este script simplemente ejecutamos el resto de playbooks en orden, con el set -e detenemos la ejecución si hay errores.
+set -e
+echo "Actualizamos y instalamos podman con nginx, levantamos también un contenedor nginx"
 ansible-playbook -i hosts Playbookpodman.yml
+
+echo "Desplegamos el Apache"
+ansible-playbook -i hosts Playbookapache.yml
+
+echo "Desplegamos ACR"
+ansible-playbook -i hosts Playbookacr.yml
+
+echo "Desplegamos en AKS con persistencia"
+ansible-playbook -i hosts Playbookpersistente.yaml
